51bf1306b55a2151bed43af2ef153bcb1cb4d6d5
Arman Jasuja <arman_jasuja@yahoo.com>
2026-09-17 12:21:43 +0100

    Added initial tests

---
+++ mars_rover/test.py
@@ -0,0 +1,17 @@
+import pytest
+from solution import MarsRover
+
+@pytest.mark.parametrize(
+    "starting_pos, starting_dir, grid_size, instructions, expected_final_pos, expected_final_dir",
+    [
+        ([0, 0], 'e', [2, 2], '', [0, 0], 'e'),
+        ([0, 0], 'e', [50, 50], 'ff', [2, 0], 'e'),
+        ([0, 0], 'e', [50, 50], 'ffrff', [2, 2], 's'),
+        
+    ]
+)
+def test_move_mars_rover(starting_pos, starting_dir, grid_size, instructions, expected_final_pos, expected_final_dir):
+    rover = MarsRover(starting_pos, starting_dir, grid_size)
+    rover.move(instructions)
+    assert rover.current_position == expected_final_pos
+    assert rover.current_direction == expected_final_dir
