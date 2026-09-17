c459e450364ba10325b2ea881453890912bd714c
Arman Jasuja <arman_jasuja@yahoo.com>
2026-09-17 12:34:06 +0100

    Refactored

--- mars_rover/solution.py
+++ mars_rover/solution.py
@@ -1,31 +1,10 @@
 class MarsRover:
-    DIRECTION_NUM_TO_LETTER = {
-        0: 'e',
-        1: 's',
-    }
+    DIRECTIONS = ['e', 's']
+    MOVEMENT_INSTRUCTIONS = {
+        'f': 'move_forward',
+        'r': 'turn_right'
     
-    DIRECTION_LETTER_TO_NUM = {
-            letter: num 
-            for num, letter in DIRECTION_NUM_TO_LETTER.items()
     }
-    
-    MOVEMENT_NUM_TO_LETTER = {
-        1: 'f',
-    }
-    
-    MOVEMENT_LETTER_TO_NUM = {
-        letter: num
-        for num, letter in MOVEMENT_NUM_TO_LETTER.items()
-    }
-    
-    ROTATION_DIRECTION_NUM_TO_LETTER = {
-        1: 'r',
-    }
-    
-    ROTATION_DIRECTION_LETTER_TO_NUM = {
-                letter: num 
-                for num, letter in ROTATION_DIRECTION_NUM_TO_LETTER.items()
-        }
     
     def __init__(self, starting_pos: list[int], starting_dir: str, grid_size: list[int]) -> None:
         self.starting_pos = starting_pos
@@ -36,7 +15,16 @@ class MarsRover:
     
     def move(self, instructions: str) -> None:
         for instruction in instructions:
-            if instruction in self.MOVEMENT_LETTER_TO_NUM:
-                self.current_position[self.DIRECTION_LETTER_TO_NUM.get(self.current_direction)] += self.MOVEMENT_LETTER_TO_NUM.get(instruction)
-            elif instruction in self.ROTATION_DIRECTION_LETTER_TO_NUM:
-                self.current_direction = self.DIRECTION_NUM_TO_LETTER.get(self.DIRECTION_LETTER_TO_NUM.get(self.current_direction) + 1)
+            if instruction in self.MOVEMENT_INSTRUCTIONS:
+                method_name = self.MOVEMENT_INSTRUCTIONS[instruction]
+                getattr(self, method_name)()
+    
+    def move_forward(self):
+        if self.current_direction == 'e':
+            self.current_position[0] += 1
+        elif self.current_direction == 's':
+            self.current_position[1] += 1
+    
+    def turn_right(self):
+        next_direction_index = self.DIRECTIONS.index(self.current_direction) + 1
+        self.current_direction = self.DIRECTIONS[next_direction_index]
